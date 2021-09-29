f59efee2b5b105662cbdb5ef473e04520ba93220
Felipe Tadeu <ftadeu@eusou.space>
2021-09-29 11:20:49 -0300

    refatorando tirando dados mokados do adm

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction, request } from "express"
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(resquest: Request, response: Response, next: NextFunction){
+export async function ensureAdmin(resquest: Request, response: Response, next: NextFunction){
 
     const { user_id } = request;
-    const admin = true;
+
+    const userRepositories = getCustomRepository(UsersRepositories);
+
+    const { admin } = await userRepositories.findOne(user_id);
 
     if(admin) {
         return next();
